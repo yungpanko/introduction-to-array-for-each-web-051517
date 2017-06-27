668bc4640359b7f96e1a76e21faa2794b20143ae
yungpanko <jrjohns@mit.edu>
2017-06-27 10:09:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-function changeCompletely(element, index, array) {
-  array[index] = element + '!!!'
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
 }
 
-function doToElementsInArray(array, callback) {
-  array.forEach(callback)
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
+
+var animals = ["dog", "cat", "squirrel"];
+
+doToElementsInArray(animals, changeCompletely);
+
+// log out animals -- pretty cool, right?
+console.log(animals);
